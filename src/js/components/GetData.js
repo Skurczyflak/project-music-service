9aec963421165f4b1c7280eff5a535f076bf8ddb
Mateusz Matysek <mateusziom1@gmail.com>
2025-05-29 17:17:29 +0200

    Change to use file Json

--- src/js/components/GetData.js
+++ src/js/components/GetData.js
@@ -5,17 +5,30 @@ class GetData {
     this.songs = [];
   }
 
+  // async getData() {
+  //   const url = settings.db.url + '/' + settings.db.songs;
+  //   try {
+  //     const response = await fetch(url);
+  //     const data = await response.json();
+  //     this.songs = data;
+  //     return data;
+  //   } catch (error) {
+  //     console.error('Could not fetch data:', error);
+  //   }
+  // }
+
   async getData() {
-    const url = settings.db.url + '/' + settings.db.songs;
-    try {
-      const response = await fetch(url);
-      const data = await response.json();
-      this.songs = data;
-      return data;
-    } catch (error) {
-      console.error('Could not fetch data:', error);
-    }
+  try {
+    const response = await fetch('db/app.json');
+    const data = await response.json();
+    this.songs = data.songs;
+    console.log(this.songs);
+    return this.songs;
+  } catch (error) {
+    console.error('Error loading data:', error);
   }
+}
+
 }
 
 export default GetData;
